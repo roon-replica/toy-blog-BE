be29cb2c46022cf6d18c257636dd0105ad04b1a3
leemr <think-roon@naver.com>
2022-10-08 23:09:25 +0900

    예약어 충돌로 인한 엔티티 네이밍 변경(h2 DB에 한정된 문제일듯)

--- src/main/java/toy/blog/be/domain/Keywords.java
+++ src/main/java/toy/blog/be/domain/Keywords.java
@@ -8,7 +8,7 @@ import javax.persistence.Embeddable;
 @Getter
 @EqualsAndHashCode // todo: 정확히 모름
 @Embeddable
-public class Keyword {
+public class Keywords {
     private String word;
 
     private int useCount;
