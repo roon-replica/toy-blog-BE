116be016b206588abb36b493e1afcd70bf7480f5
leemr <think-roon@naver.com>
2022-10-08 23:56:41 +0900

    swagger는 권한 상관없이 접근가능하도록 변경

--- src/main/java/toy/blog/be/config/WebSecurityConfig.java
+++ src/main/java/toy/blog/be/config/WebSecurityConfig.java
@@ -34,7 +34,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		http.authorizeRequests().antMatchers("/login", "/signup", "/user").permitAll() // 누구든 접근 허용
+		http.authorizeRequests().antMatchers("/login", "/signup", "/user","/swagger-ui.html").permitAll() // 누구든 접근 허용
 			.antMatchers("/").hasRole("USER") // USER, ADMIN만 접근 가능
 			.antMatchers("/admin").hasRole("ADMIN") // ADMIN만 접근 가능
 			.anyRequest().authenticated() // 나머지는 권한 종류 상관 없이 권한 있어야 접근 가능
