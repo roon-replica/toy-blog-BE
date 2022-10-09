bc2a6e8cc01714a253e343e4133a637333f66ea3
leemr <think-roon@naver.com>
2022-10-09 23:27:35 +0900

    접근 제어 제거

--- src/main/java/toy/blog/be/config/WebSecurityConfig.java
+++ src/main/java/toy/blog/be/config/WebSecurityConfig.java
@@ -15,13 +15,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
-		//auth.userDetailsService(userService);
-		
-		//super.configure(auth);
-	}
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        //auth.userDetailsService(userService);
+
+        //super.configure(auth);
+    }
 
 //	@Override
 //	public void configure(WebSecurity web) throws Exception {
@@ -31,9 +31,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //		//super.configure(web);
 //	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		 http.authorizeRequests().antMatchers("/login", "/signup", "/user","/swagger-ui.html").permitAll(); // 누구든 접근 허용
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//		 http.authorizeRequests().antMatchers("/login", "/signup", "/user","/swagger-ui.html").permitAll(); // 누구든 접근 허용
 //			.antMatchers("/").hasRole("USER") // USER, ADMIN만 접근 가능
 //			.antMatchers("/admin").hasRole("ADMIN") // ADMIN만 접근 가능
 //			.anyRequest().authenticated() // 나머지는 권한 종류 상관 없이 권한 있어야 접근 가능
@@ -41,13 +41,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //			.formLogin().loginPage("/login").defaultSuccessUrl("/")
 //			.and()
 //			.logout().logoutSuccessUrl("/login").invalidateHttpSession(true);
-		
-		//super.configure(http);
-	}
 
-	@Bean
-	public PasswordEncoder passwordEncoder(){
-		return new BCryptPasswordEncoder();
-	}
-	
+        //super.configure(http);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
