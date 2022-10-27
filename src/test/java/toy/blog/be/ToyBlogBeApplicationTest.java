e9c3c61fc55a44fd4cca4561699f3721b460b9da
roon <think-roon@naver.com>
2022-10-27 22:39:29 +0900

    resolve test class path issue

--- src/test/java/toy/blog/be/ToyBlogBeApplicationTest.java
+++ src/test/java/toy/blog/be/ToyBlogBeApplicationTest.java
@@ -1,12 +1,10 @@
 package toy.blog.be;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ToyBlogBeApplicationTest {
-
     @Test
     void contextLoads() {
     }
+
 }
