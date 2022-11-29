a111e4f36ef76fd1f6872c84309782183ef38511
leemr <think-roon@naver.com>
2022-11-30 00:11:12 +0900

    #62 UserInfo 통합 놓친 부분 수정

--- src/main/java/toy/blog/be/repository/FollowRepositoryImpl.java
+++ src/main/java/toy/blog/be/repository/FollowRepositoryImpl.java
@@ -5,9 +5,9 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
+import toy.blog.be.domain.entity.QUserInfo;
 import toy.blog.be.domain.entity.UserInfo;
 import toy.blog.be.domain.entity.QFollow;
-import toy.blog.be.domain.entity.QOAuthUserInfo;
 
 import java.util.List;
 
@@ -18,7 +18,7 @@ public class FollowRepositoryImpl {
     private final JPAQueryFactory jpaQueryFactory;
 
     QFollow qFollow = QFollow.follow;
-    QOAuthUserInfo qoAuthUserInfo = QOAuthUserInfo.oAuthUserInfo;
+    QUserInfo qoAuthUserInfo = QUserInfo.userInfo;
 
     public List<UserInfo> getFollower(String followeeId) {
         return jpaQueryFactory
